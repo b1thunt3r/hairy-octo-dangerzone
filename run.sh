ef6efbf31f553cd57fb77ff32c354ddd3fc87568
Ishan Jain <ishan.jain@avensia.com>
2018-01-26 14:09:11 +0100

    update19 run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #curl -sS -o dotnet.tgz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-linux-x64.tar.gz
 #tar -xf dotnet.tgz
-tar -xf app.tgz
+#tar -xf app.tgz
 
 #cp ./shared/Microsoft.NETCore.App/2.0.5/libhostpolicy.so app/
 #cp ./host/fxr/2.0.5/libhostfxr.so app/
