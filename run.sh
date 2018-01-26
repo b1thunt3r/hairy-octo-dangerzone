143b5f049194a32a747274175231b52c4d9dd762
Ishan Jain <ishan.jain@avensia.com>
2018-01-26 13:45:37 +0100

    update12 run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./app/
+curl -sS https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-linux-x64.tar.gz | tar -xv
+tar app.tgz
 
-chmod a+x app/ConsoleApp1.dll
-./app/ConsoleApp1.dll
+cp ./shared/Microsoft.NETCore.App/2.0.5/libhostpolicy.so app/
+cp ./host/fxr/2.0.5/libhostfxr.so app/
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./app/:./shared/Microsoft.NETCore.App/2.0.5/:./
+
+./dotnet app/ConsoleApp1.dll
