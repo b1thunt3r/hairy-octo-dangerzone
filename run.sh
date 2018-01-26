5f54ab8abb67cac024830a010c7d944ab9126f74
Ishan Jain <ishan.jain@avensia.com>
2018-01-26 13:01:51 +0100

    update6 run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 curl -sS -o dotnet.tgz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-linux-x64.tar.gz
 tar -xf dotnet.tgz
 
-ldd -v app/ConsoleApp1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./
 
-./dotnet --info > b.txt
-#./dotnet app/ConsoleApp1.dll
+pwd
+
+#ldd -v app/ConsoleApp1
+
+#./dotnet --info > b.txt
+./dotnet app/ConsoleApp1.dll
