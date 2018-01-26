4f5419de059eb6632a1312507fab8fd728f0c667
Ishan Jain <ishan.jain@avensia.com>
2018-01-26 12:42:54 +0100

    added run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+curl -o dotnet.tgz https://download.microsoft.com/download/1/1/0/11046135-4207-40D3-A795-13ECEA741B32/dotnet-runtime-2.0.5-linux-x64.tar.gz
+tar -xvfz dotnet.tgz
+dotnet run app/ConsoleApp1.dll
